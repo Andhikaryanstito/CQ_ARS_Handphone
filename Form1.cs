2e43deb43c200a8a507a3dbd83856b8de7517c6c
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-03-31 10:19:53 +0700

    Implementasi ExecuteScalar untuk menghitung jumlah Mahasiswa

--- Form1.cs
+++ Form1.cs
@@ -30,9 +30,31 @@ namespace PraktikumADO
         {
             try
             {
-                Koneksi(); 
+                Koneksi();
+                conn.Open();
+                MessageBox.Show("Koneksi ke database berhasil!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnHitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
                 conn.Open(); 
-                MessageBox.Show("Koneksi ke database berhasil!"); 
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
                 conn.Close(); 
             }
             catch (Exception ex)
