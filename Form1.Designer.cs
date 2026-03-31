765fd5e67f77751658a6ca4d479aa2894070b3a4
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2026-03-31 11:01:05 +0700

    Implementasi ExecuteNonQuery untuk update alamat Mahasiswa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -98,6 +98,7 @@
             this.btnUpdate.TabIndex = 6;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // Form1
             // 
